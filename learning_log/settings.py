eff9d046ea8453ea8a588f2e74da70ad6d7f215f
elajth <johan.lith@gmail.com>
2023-11-12 23:24:51 +0100

    changes

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -80,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'learning_log.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -90,7 +89,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

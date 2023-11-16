5ff77d62250cbd5b022d8f1835f5ad238968eec4
elajth <johan.lith@gmail.com>
2023-11-16 13:24:18 +0100

    changed to pgsql

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'learning_log2',
+        'USER': 'elajth',
+        'PASSWORD': 'xq9TYrsY',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
